c6d474da8c3a985de29df6d5e60ea3f548d69893
ConradWilliam <mcwilliam131@gmail.com>
2022-11-27 15:45:05 +0300

    new code that makes chart for individual date

--- Personal_Scheduler_v2/GanntChart.py
+++ Personal_Scheduler_v2/GanntChart.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import datetime as dt
 from SortAlgos2 import returntasks 
 
-#
+#git
 TaskCompiler = returntasks()
 
 def TaskDict(list):
@@ -21,9 +21,21 @@ dl_df = pd.DataFrame(TaskDictList)
 dl_df['width'] = (dl_df.timeend) - (dl_df.timestart)
 # print(dl_df)
 
-plt.barh(y=dl_df['task'], width=dl_df['width'], left=dl_df['timestart'])
-plt.title("Gannt Chart Showing Tasks In Accordance With Their Deadlines")
-plt.xlabel("Duration For Tasks")
-plt.ylabel("Tasks At Hand")
+def DateChart(date):
+    date_df = dl_df.loc[dl_df["dueDate"] == date]
+    print(date_df)
+    plt.barh(y=date_df['task'], width=date_df['width'], left=date_df['timestart'])
+    plt.title("Gannt Chart Showing Tasks In Accordance With Their Deadlines")
+    plt.xlabel("Duration For Tasks")
+    plt.ylabel("Tasks At Hand")
+    plt.savefig('Gannt Chart For Tasks.png')
 
-plt.savefig('Gannt Chart For Tasks.png')
+
+DateChart(15)
+
+# plt.barh(y=dl_df['task'], width=dl_df['width'], left=dl_df['timestart'])
+# plt.title("Gannt Chart Showing Tasks In Accordance With Their Deadlines")
+# plt.xlabel("Duration For Tasks")
+# plt.ylabel("Tasks At Hand")
+
+# plt.savefig('Gannt Chart For Tasks.png')
